c4e47034838a980890e37d77a14747c70103448c
qingchunyibeifangzongle <yuepeng1@110tal.com>
2020-04-16 16:37:05 +0800

    create sentry yach plugin

---
+++ README.md
@@ -0,0 +1,21 @@
+# Sentry-Yach
+
+`Sentry`的`yach`通知插件
+
+## 安装
+
+```bash
+$ pip install sentry-yach
+```
+
+## 使用
+
+在`项目`的所有集成页面找到`Yach`插件，启用，并设置`Access Token`
+
+![plugin](https://github.com/qingchunyibeifangzongle/sentry-yach-plugin/blob/master/docs/images/options.png)
+
+在插件上使用`Test Plugin`进行测试，当配置好`Access Token`后，在 yach 群内会得到以下警告
+
+![plugin](https://github.com/qingchunyibeifangzongle/sentry-yach-plugin/blob/master/docs/images/yach.png)
+
+点击`href`按钮，打开异常详情页面。
---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="sentry-yach-plugin",
+    version='0.0.1',
+    author='qingchunyibeifangzongle',
+    author_email='xiaoyueueyue0612@gmail.com',
+    url='https://github.com/qingchunyibeifangzongle/sentry-yach-plugin',
+    description='A Sentry extension which send errors stats to yach',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    keywords='sentry yach',
+    include_package_data=True,
+    zip_safe=False,
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=[
+        'sentry>=9.0.0',
+        'requests',
+    ],
+    entry_points={
+        'sentry.plugins': [
+            'sentry_yach_plugin = sentry_yach_plugin.plugin:YachPlugin'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        "License :: OSI Approved :: MIT License",
+    ]
+)
